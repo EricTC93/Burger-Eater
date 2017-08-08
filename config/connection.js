948a849997a81bb7e0a7a108d3fcd329b7e891fd
YETC7 <tyler_726@live.com>
2017-08-08 03:03:14 -0700

    connects to mysql database

---
+++ config/connection.js
@@ -0,0 +1,26 @@
+// Dependecies
+var mysql = require("mysql");
+
+// Connection information
+var connection = mysql.createConnection({
+  port: 3306,
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "burger_db"
+});
+
+// Connect to the database
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId);
+});
+
+connection.query("SELECT * FROM burgers", function(err, res) {
+    if (err) throw err;
+
+    console.log(res);
+});
+
+// Export connection
+module.exports = connection;
---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+DROP DATABASE IF EXISTS burger_db;
+CREATE DATABASE burger_db;
+USE burger_db;
+
+CREATE TABLE burgers (
+	id INTEGER(11) AUTO_INCREMENT NOT NULL,
+    burger_name VARCHAR(50),
+    devoured BOOLEAN DEFAULT FALSE,
+    date TIMESTAMP DEFAULT NOW(),
+    PRIMARY KEY(id)
+);
